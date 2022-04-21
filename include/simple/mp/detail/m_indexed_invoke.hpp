db36753acc24339f6151187d83f8d0c756f06dd2
Jonas Andersen <jonasna93@gmail.com>
2022-04-21 18:14:05 +0200

    improved indexed invoke

--- include/simple/mp/detail/m_indexed_invoke.hpp
+++ include/simple/mp/detail/m_indexed_invoke.hpp
@@ -64,121 +64,167 @@ struct indexed_invoke<1> {
     }                                                                                                                  \
   };
 
+#define __II_C5__(offset)                                                                                              \
+  __II_C__(0 + offset);                                                                                                \
+  __II_C__(1 + offset);                                                                                                \
+  __II_C__(2 + offset);                                                                                                \
+  __II_C__(3 + offset);                                                                                                \
+  __II_C__(4 + offset)
+
+#define __II_C10__(offset)                                                                                             \
+  __II_C5__(0 + offset);                                                                                               \
+  __II_C5__(5 + offset)
+
+#define __II_C20__(offset)                                                                                             \
+  __II_C10__(0 + offset);                                                                                              \
+  __II_C10__(10 + offset)
+
+#define __II_C30__(offset)                                                                                             \
+  __II_C20__(0 + offset);                                                                                              \
+  __II_C10__(20 + offset)
+
+#define __II_C40__(offset)                                                                                             \
+  __II_C20__(0 + offset);                                                                                              \
+  __II_C20__(20 + offset)
+
+#define __II_C50__(offset)                                                                                             \
+  __II_C20__(0 + offset);                                                                                              \
+  __II_C30__(20 + offset)
+
 __II__(2, __II_C__(0); __II_C__(1))
 
 __II__(3, __II_C__(0); __II_C__(1); __II_C__(2))
 
 __II__(4, __II_C__(0); __II_C__(1); __II_C__(2); __II_C__(3))
 
-__II__(5, __II_C__(0); __II_C__(1); __II_C__(2); __II_C__(3); __II_C__(4))
+__II__(5, __II_C5__(0))
 
-__II__(6, __II_C__(0); __II_C__(1); __II_C__(2); __II_C__(3); __II_C__(4); __II_C__(5))
+__II__(6, __II_C5__(0); __II_C__(5))
 
-__II__(7, __II_C__(0); __II_C__(1); __II_C__(2); __II_C__(3); __II_C__(4); __II_C__(5); __II_C__(6))
+__II__(7, __II_C5__(0); __II_C__(5); __II_C__(6))
 
-__II__(8, __II_C__(0); __II_C__(1); __II_C__(2); __II_C__(3); __II_C__(4); __II_C__(5); __II_C__(6); __II_C__(7))
+__II__(8, __II_C5__(0); __II_C__(5); __II_C__(6); __II_C__(7))
 
-__II__(9, __II_C__(0); __II_C__(1); __II_C__(2); __II_C__(3); __II_C__(4); __II_C__(5); __II_C__(6); __II_C__(7);
-       __II_C__(8))
+__II__(9, __II_C5__(0); __II_C__(5); __II_C__(6); __II_C__(7); __II_C__(8))
 
-__II__(10, __II_C__(0); __II_C__(1); __II_C__(2); __II_C__(3); __II_C__(4); __II_C__(5); __II_C__(6); __II_C__(7);
-       __II_C__(8);
-       __II_C__(9))
+__II__(10, __II_C10__(0))
 
-__II__(11, __II_C__(0); __II_C__(1); __II_C__(2); __II_C__(3); __II_C__(4); __II_C__(5); __II_C__(6); __II_C__(7);
-       __II_C__(8);
-       __II_C__(9);
-       __II_C__(10))
+// --
 
-__II__(12, __II_C__(0); __II_C__(1); __II_C__(2); __II_C__(3); __II_C__(4); __II_C__(5); __II_C__(6); __II_C__(7);
-       __II_C__(8);
-       __II_C__(9);
-       __II_C__(10);
-       __II_C__(11))
+__II__(11, __II_C10__(0); __II_C__(10))
 
-__II__(13, __II_C__(0); __II_C__(1); __II_C__(2); __II_C__(3); __II_C__(4); __II_C__(5); __II_C__(6); __II_C__(7);
-       __II_C__(8);
-       __II_C__(9);
-       __II_C__(10);
-       __II_C__(11);
-       __II_C__(12))
+__II__(12, __II_C10__(0); __II_C__(10); __II_C__(11))
 
-__II__(14, __II_C__(0); __II_C__(1); __II_C__(2); __II_C__(3); __II_C__(4); __II_C__(5); __II_C__(6); __II_C__(7);
-       __II_C__(8);
-       __II_C__(9);
-       __II_C__(10);
-       __II_C__(11);
-       __II_C__(12);
-       __II_C__(13))
+__II__(13, __II_C10__(0); __II_C__(10); __II_C__(11); __II_C__(12))
 
-__II__(15, __II_C__(0); __II_C__(1); __II_C__(2); __II_C__(3); __II_C__(4); __II_C__(5); __II_C__(6); __II_C__(7);
-       __II_C__(8);
-       __II_C__(9);
-       __II_C__(10);
-       __II_C__(11);
-       __II_C__(12);
-       __II_C__(13);
-       __II_C__(14))
+__II__(14, __II_C10__(0); __II_C__(10); __II_C__(11); __II_C__(12); __II_C__(13))
 
-__II__(16, __II_C__(0); __II_C__(1); __II_C__(2); __II_C__(3); __II_C__(4); __II_C__(5); __II_C__(6); __II_C__(7);
-       __II_C__(8);
-       __II_C__(9);
-       __II_C__(10);
-       __II_C__(11);
-       __II_C__(12);
-       __II_C__(13);
-       __II_C__(14);
-       __II_C__(15))
+__II__(15, __II_C10__(0); __II_C5__(10))
 
-__II__(17, __II_C__(0); __II_C__(1); __II_C__(2); __II_C__(3); __II_C__(4); __II_C__(5); __II_C__(6); __II_C__(7);
-       __II_C__(8);
-       __II_C__(9);
-       __II_C__(10);
-       __II_C__(11);
-       __II_C__(12);
-       __II_C__(13);
-       __II_C__(14);
-       __II_C__(15);
-       __II_C__(16))
+__II__(16, __II_C10__(0); __II_C5__(10); __II_C__(15))
 
-__II__(18, __II_C__(0); __II_C__(1); __II_C__(2); __II_C__(3); __II_C__(4); __II_C__(5); __II_C__(6); __II_C__(7);
-       __II_C__(8);
-       __II_C__(9);
-       __II_C__(10);
-       __II_C__(11);
-       __II_C__(12);
-       __II_C__(13);
-       __II_C__(14);
-       __II_C__(15);
-       __II_C__(16);
-       __II_C__(17))
+__II__(17, __II_C10__(0); __II_C5__(10); __II_C__(15); __II_C__(16))
 
-__II__(19, __II_C__(0); __II_C__(1); __II_C__(2); __II_C__(3); __II_C__(4); __II_C__(5); __II_C__(6); __II_C__(7);
-       __II_C__(8);
-       __II_C__(9);
-       __II_C__(10);
-       __II_C__(11);
-       __II_C__(12);
-       __II_C__(13);
-       __II_C__(14);
-       __II_C__(15);
-       __II_C__(16);
-       __II_C__(17);
-       __II_C__(18))
+__II__(18, __II_C10__(0); __II_C5__(10); __II_C__(15); __II_C__(16); __II_C__(17))
 
-__II__(20, __II_C__(0); __II_C__(1); __II_C__(2); __II_C__(3); __II_C__(4); __II_C__(5); __II_C__(6); __II_C__(7);
-       __II_C__(8);
-       __II_C__(9);
-       __II_C__(10);
-       __II_C__(11);
-       __II_C__(12);
-       __II_C__(13);
-       __II_C__(14);
-       __II_C__(15);
-       __II_C__(16);
-       __II_C__(17);
-       __II_C__(18);
-       __II_C__(19))
+__II__(19, __II_C10__(0); __II_C5__(10); __II_C__(15); __II_C__(16); __II_C__(17); __II_C__(18))
+
+__II__(20, __II_C10__(0); __II_C10__(10))
+
+// --
+
+__II__(21, __II_C20__(0); __II_C__(20))
+
+__II__(22, __II_C20__(0); __II_C__(20); __II_C__(21))
+
+__II__(23, __II_C20__(0); __II_C__(20); __II_C__(21); __II_C__(22))
+
+__II__(24, __II_C20__(0); __II_C__(20); __II_C__(21); __II_C__(22); __II_C__(23))
+
+__II__(25, __II_C20__(0); __II_C5__(20))
+
+__II__(26, __II_C20__(0); __II_C5__(20); __II_C__(25))
+
+__II__(27, __II_C20__(0); __II_C5__(20); __II_C__(25); __II_C__(26))
+
+__II__(28, __II_C20__(0); __II_C5__(20); __II_C__(25); __II_C__(26); __II_C__(27))
+
+__II__(29, __II_C20__(0); __II_C5__(20); __II_C__(25); __II_C__(26); __II_C__(27); __II_C__(28))
+
+__II__(30, __II_C30__(0))
+
+// --
+
+__II__(31, __II_C30__(0); __II_C__(30))
+
+__II__(32, __II_C30__(0); __II_C__(30); __II_C__(31))
+
+__II__(33, __II_C30__(0); __II_C__(30); __II_C__(31); __II_C__(32))
+
+__II__(34, __II_C30__(0); __II_C__(30); __II_C__(31); __II_C__(32); __II_C__(33))
+
+__II__(35, __II_C30__(0); __II_C5__(30))
+
+__II__(36, __II_C30__(0); __II_C5__(30); __II_C__(35))
+
+__II__(37, __II_C30__(0); __II_C5__(30); __II_C__(35); __II_C__(36))
+
+__II__(38, __II_C30__(0); __II_C5__(30); __II_C__(35); __II_C__(36); __II_C__(37))
+
+__II__(39, __II_C30__(0); __II_C5__(30); __II_C__(35); __II_C__(36); __II_C__(37); __II_C__(38))
+
+__II__(40, __II_C40__(0))
+
+// --
+
+__II__(41, __II_C40__(0); __II_C__(40))
+
+__II__(42, __II_C40__(0); __II_C__(40); __II_C__(41))
+
+__II__(43, __II_C40__(0); __II_C__(40); __II_C__(41); __II_C__(42))
+
+__II__(44, __II_C40__(0); __II_C__(40); __II_C__(41); __II_C__(42); __II_C__(43))
+
+__II__(45, __II_C40__(0); __II_C5__(40))
+
+__II__(46, __II_C40__(0); __II_C5__(40); __II_C__(45))
+
+__II__(47, __II_C40__(0); __II_C5__(40); __II_C__(45); __II_C__(46))
+
+__II__(48, __II_C40__(0); __II_C5__(40); __II_C__(45); __II_C__(46); __II_C__(47))
+
+__II__(49, __II_C40__(0); __II_C5__(40); __II_C__(45); __II_C__(46); __II_C__(47); __II_C__(48))
+
+__II__(50, __II_C50__(0))
+
+// --
+
+__II__(51, __II_C50__(0); __II_C__(50))
+
+__II__(52, __II_C50__(0); __II_C__(50); __II_C__(51))
+
+__II__(53, __II_C50__(0); __II_C__(50); __II_C__(51); __II_C__(52))
+
+__II__(54, __II_C50__(0); __II_C__(50); __II_C__(51); __II_C__(52); __II_C__(53))
+
+__II__(55, __II_C50__(0); __II_C5__(50))
+
+__II__(56, __II_C50__(0); __II_C5__(50); __II_C__(55))
+
+__II__(57, __II_C50__(0); __II_C5__(50); __II_C__(55); __II_C__(56))
+
+__II__(58, __II_C50__(0); __II_C5__(50); __II_C__(55); __II_C__(56); __II_C__(57))
+
+__II__(59, __II_C50__(0); __II_C5__(50); __II_C__(55); __II_C__(56); __II_C__(57); __II_C__(58))
+
+__II__(60, __II_C50__(0); __II_C10__(50))
+
+#undef __II_C5__
+#undef __II_C10__
+#undef __II_C20__
+#undef __II_C30__
+#undef __II_C40__
+#undef __II_C50__
 
 #undef __II__
 #undef __II_C__
@@ -217,7 +263,6 @@ struct composed_invoke_impl;
 
 template <std::size_t N, std::size_t... Is>
 struct composed_invoke_impl<N, Is...> {
-
   template <typename I, typename Function, typename... IndexTypes, typename... Args>
   constexpr decltype(auto) operator()(I, std::tuple<IndexTypes...> const& it, Function&& function, Args&&... args)
   {
@@ -225,7 +270,10 @@ struct composed_invoke_impl<N, Is...> {
     return m_indexed_invoke<N>(
         std::get<0>(it), composed_invoke_impl<Is...>{}, tuple_pop_front(it), fn, std::forward<Args>(args)...);
   }
+};
 
+template <std::size_t N>
+struct composed_invoke_impl<N> {
   template <typename I, typename Function, typename IndexType, typename... Args>
   constexpr decltype(auto) operator()(I, std::tuple<IndexType> const& it, Function&& function, Args&&... args)
   {
@@ -242,6 +290,7 @@ struct composed_invoke_impl<> {
     return std::forward<Function>(function)(I{}, std::forward<Args>(args)...);
   }
 };
+
 }  // namespace detail
 
 template <std::size_t N, typename Function, typename... Args>
@@ -254,7 +303,7 @@ inline constexpr decltype(auto) m_indexed_invoke(std::size_t index, Function&& f
 
 template <std::size_t N, std::size_t... Ns, typename I1, typename... Is, typename Function, typename... Args>
 inline constexpr decltype(auto)
-m_multi_indexed_invoke(std::tuple<I1, Is...> const& indexes, Function&& function, Args&&... args)
+m_indexed_invoke(std::tuple<I1, Is...> const& indexes, Function&& function, Args&&... args)
 {
   static_assert(sizeof...(Ns) == sizeof...(Is), "");
   return m_indexed_invoke<N>(
--- test/smp_tests.cpp
+++ test/smp_tests.cpp
@@ -1,32 +1,20 @@
 #include <iostream>
 
-#include <simple/mp/type_name.hpp>
-
 #include <simple/mp.hpp>
 
 using namespace simple::mp;
-using std::tuple;
 
-using L1 = std::tuple<int, std::tuple<>, void, std::tuple<float, double>>;
-using R1 = m_flatten<L1>;  // tuple<int, void, float, double>
-
-using L2 = m_list<int, m_list<float>, tuple<void>>;
-using R2a = m_flatten<L2>;           // mp_list<int, float, tuple<void>>
-using R2b = m_flatten<L2, tuple<>>;  // mp_list<int, mp_list<float>, void>
-
-using L3 = m_list<m_list<float>, m_list<m_list<void>>>;
-using R3 = m_flatten<L3>;  // mp_list<float, mp_list<void>>
-                           //
 int main()
 {
-  std::cout << "R1:" << m_type_name<R1>() << std::endl;
-  std::cout << "R2a: " << m_type_name<R2a>() << std::endl;
-  std::cout << "R2b: " << m_type_name<R2b>() << std::endl;
-  std::cout << "R3 " << m_type_name<R3>() << std::endl;
-  // m_indexed_vtable_invoke_c<10>(5, [](auto I) {});
-  m_indexed_invoke<10>(5, [](auto I) { std::cout << I.value << std::endl; });
-
-  constexpr auto ret = m_multi_indexed_invoke<10, 5, 2>(
-      std::make_tuple(1, 0, 1), [](auto I1, auto I2, auto I3) { return I1.value + I2.value + I3.value; });
+  auto ret = m_indexed_invoke<20, 52, 100>(
+      std::make_tuple(16, 50, 99),
+      [](auto I1, auto I2, auto I3, auto const* msg)
+      {
+        std::cout << msg << std::endl;
+        return I1.value + I2.value + I3.value;
+      },
+      "Hello");
   std::cout << ret << std::endl;
+  // auto ret = m_indexed_invoke<2546>(732, [](auto I) { return I.value; });
+  // std::cout << ret << std::endl;
 }
